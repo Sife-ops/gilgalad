4dcca0ddbd8b910c89694b7f69ca03f9ad71294b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-03-15 23:28:55 -0700

    rename stack

--- stacks/GilgaladStack.ts
+++ stacks/GilgaladStack.ts
@@ -2,12 +2,12 @@ import * as sst from "@serverless-stack/resources";
 
 const { DOMAIN, SUBDOMAIN } = process.env;
 
-export default class MyStack extends sst.Stack {
+export default class Gilgalad extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props);
 
     // Create the table
-    const table = new sst.Table(this, "Counter", {
+    const table = new sst.Table(this, `table`, {
       fields: {
         counter: sst.TableFieldType.STRING,
       },
@@ -15,7 +15,7 @@ export default class MyStack extends sst.Stack {
     });
 
     // Create the HTTP API
-    const api = new sst.Api(this, "Api", {
+    const api = new sst.Api(this, "api", {
       defaultFunctionProps: {
         // Pass in the table name to our API
         environment: {
@@ -31,8 +31,7 @@ export default class MyStack extends sst.Stack {
     api.attachPermissions([table]);
 
     // Deploy our React app
-    // const domain = scope.stage === "prod" ? "prod0" : "dev0";
-    const site = new sst.ViteStaticSite(this, "ViteSite", {
+    const site = new sst.ViteStaticSite(this, "site", {
       path: "frontend",
       environment: {
         // Pass in the API endpoint to our app
--- stacks/index.ts
+++ stacks/index.ts
@@ -1,11 +1,10 @@
-import MyStack from "./MyStack";
+import GilgaladStack from "./GilgaladStack";
 import * as sst from "@serverless-stack/resources";
 
 export default function main(app: sst.App): void {
-  // Set default runtime for all functions
   app.setDefaultFunctionProps({
     runtime: "nodejs14.x",
   });
 
-  new MyStack(app, "my-stack");
+  new GilgaladStack(app, "stack");
 }
